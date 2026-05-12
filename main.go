b44d5a1c607514f2f4c0805afc96f869bec4f71a
Andrey Plotnikov <andrejj.plotnikov@vitech.team>
2026-05-12 15:11:17 +0300

    feat changes v0.0.1

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
-	"hexlet-go/greeting"
+	"github.com/Violet-Lemon/hexlet-go/greeting"
+	"github.com/fatih/color"
 )
 
 func main() {
+	color.Cyan(greeting.Hello())
+	color.Blue("I'm coloured text.")
+	color.Magenta("Good luck!")
 	fmt.Println(greeting.Hello())
 }
